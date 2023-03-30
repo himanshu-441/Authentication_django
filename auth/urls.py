0de12f6d73a4a6870644277d68c7a315e6f33162
Himanshu <hs127010@gmail.com>
2023-03-31 00:11:32 +0530

    added view based authentication

--- auth/urls.py
+++ auth/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from auth import views
 
 urlpatterns = [
-    path('login/', views.login)
+    path('login/', views.Login.as_view()),
+    path('logout/', views.LogoutView.as_view())
+    #path('login/', views.login)
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is a authenticated view')
+    return render(request, 'main/index.html')
+
+
+@login_required(login_url='/auth/login')
+def sshh(request):
+    return HttpResponse("This is a authenticated view !")
